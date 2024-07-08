3516009765bf2eb07b89deae1e9297e46bd099eb
Jamie Hirvonen <hirvonenjamie@gmail.com>
2024-07-08 22:19:13 +1000

    Started working on multiple mechanics: timer, spillage tracker, drink tracker, etc.

--- Assets/Scripts/Minigames/DrinkingGame.cs
+++ Assets/Scripts/Minigames/DrinkingGame.cs
@@ -4,50 +4,99 @@ using UnityEngine;
 
 public class DrinkingGame : MonoBehaviour
 {
-    private int timer;
+    //Varaibles for points and stats tracking
     private int regularDrank;
     private int goldenDrank;
-    private int spillageAmount;
+    private int totalPoints;
+    private float totalDrank;
 
+    //Variables for tankard generation
     private int randomNum;
     private int randomMax;
     private int sinceGolden;
 
-    private void Awake()
+    //Value on balance meter, much like a speedometer
+    private int balanceLevel;
+    private bool inDrinkZone;
+    private bool inBoostZone;
+    private bool inSpillZone;
+    private float spillageAmount;
+
+    private float amountLeft;
+
+    void Start()
     {
         GameSetup();
     }
+
+    private void Update()
+    {        
+        if (inSpillZone)
+        {
+            //Gradually increase spill meter
+            spillageAmount +=  Time.deltaTime;
+        }
+
+        if (inDrinkZone)
+        {
+            //Gradually decrease amount in tankard
+            amountLeft -= Time.deltaTime;
+        }
+
+        if (inBoostZone)
+        {
+            //Decrease amount in tankard by a larger value
+            amountLeft -= 2 * Time.deltaTime;
+        }
+    }
+
+    private void updateTimer(float timer)
+    {
+        timer -= Time.deltaTime;
+
+        float minutes = Mathf.FloorToInt(timer / 60);
+        float seconds = Mathf.FloorToInt(timer % 60);
+    }
+
     private void GameSetup()
     {
         randomMax = 1;
         sinceGolden = 0;
+
+        inDrinkZone = false;
+        inSpillZone = false;
+
+        spillageAmount = 0f;
+
+        timer = 60.00f;
     }
 
     //Generates tankard order
     private void TankardGenerator(bool isGolden)
     {
-        randomNum = Random.Range(1, randomMax);
         //This will pick a random number, but by having the max be a variable it will allow for the chances to be easily changed
-
+        randomNum = Random.Range(1, randomMax);
+        
         if (randomNum == 1)
         {
             isGolden = false;
 
-            sinceGolden += 1;
             //Increases the count everytime a tankard isnt chosen to be golden
-            randomMax = (5 - sinceGolden);
+            sinceGolden += 1;
+
             //Every time a normal tankard is chosen, the chance for a golden tankard increases by 20%
+            randomMax = (5 - sinceGolden);  
         }
         else
         {
             isGolden = true;
 
-            sinceGolden = 0;
             //Since a golden tankard was chosen, the count is reset
-            randomMax = 1;
-            //Any time a tankard is selected to be golden, the next one is guaranteed to be normal
-        }
+            sinceGolden = 0;
 
+            //Any time a tankard is selected to be golden, the next one is guaranteed to be normal
+            randomMax = 1;
+        }
     }
 
     //Handles balance mechanic
@@ -57,8 +106,4 @@ public class DrinkingGame : MonoBehaviour
     {
 
     }
-
-    //Totals points throughout game
-
-    // Start is called before the first frame update
 }
