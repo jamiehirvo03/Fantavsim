80045433ba7c25836d237ab53a1af1f393743748
Jamie <hirvonenjamie@gmail.com>
2024-07-19 15:27:34 +1000

    Tankard generation mechanic complete, Beginning work for balance mechanic and UI implementation.

--- Assets/Scripts/Minigames/DrinkingGame.cs
+++ Assets/Scripts/Minigames/DrinkingGame.cs
@@ -1,15 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DrinkingGame : MonoBehaviour
 {
     //Varaibles for points and stats tracking
-    private bool isGolden;
-    private int regularDrank;
-    private int goldenDrank;
-    private int totalPoints;
-    private float totalDrank;
+    [SerializeField] private int regularDrank;
+    [SerializeField] private int goldenDrank;
+    [SerializeField] private int totalPoints;
+    [SerializeField] private float totalDrank;
 
     //Variables for tankard generation
     private int randomNum;
@@ -17,11 +18,16 @@ public class DrinkingGame : MonoBehaviour
     private int sinceGolden;
 
     //Value on balance meter, much like a speedometer
-    private int balanceLevel;
-    private float spillageAmount;
-    private float amountLeft;
+    [SerializeField] private float balanceLevel;
+    
+    //Multiplies the standard deltatime for greater decay rate
+    public float decayRate = 5f;
 
-    [SerializeField] private List<string> TankardOrder = new List<string>();
+    [SerializeField] private float spillageAmount;
+    [SerializeField] private float amountLeft;
+
+    [SerializeField] private List<bool> IsGolden = new List<bool>(5);
+    private bool currentListValue;
     private enum BalanceState
     {
         Idle,
@@ -32,6 +38,9 @@ public class DrinkingGame : MonoBehaviour
 
     private BalanceState currentState;
 
+    //Bool to check if the setup steps have happened
+    private bool gameIsSetup = false;
+
     void Start()
     {
         //Events that listen when the game tutorial has been displayed and player has clicked start
@@ -39,6 +48,7 @@ public class DrinkingGame : MonoBehaviour
         EventManager.current.onDrinkingGameTimeOver += OnTimeOver;
     }
 
+    //Listens to when the start button is pressed (to start the game once rules have been read)
     private void OnStartDrinkingGame()
     {
         GameSetup();
@@ -54,19 +64,52 @@ public class DrinkingGame : MonoBehaviour
     }
 
     private void Update()
-    {        
-        if (amountLeft <= 0)
+    {
+        if (gameIsSetup)
         {
-            if (isGolden)
+            if (amountLeft <= 0)
             {
-                goldenDrank++;
-                NextDrink();
+                if (IsGolden[0] == true)
+                {
+                    goldenDrank++;
+                    ClearDrink();
+                    NextDrink();
 
+                }
+                else
+                {
+                    regularDrank++;
+                    ClearDrink();
+                    NextDrink();
+                }
             }
-            else
+            
+            if (balanceLevel >= 0)
             {
-                regularDrank++;
-                NextDrink();
+                //Constantly lower the balance meter
+                balanceLevel -= decayRate * Time.deltaTime;
+
+                if (balanceLevel < 100)
+                {
+                    if (Input.GetKeyDown(KeyCode.Space))
+                    {
+                        Debug.Log("Player has pressed SPACE");
+
+                        balanceLevel += 10f;
+                    }
+                }  
+            }
+
+            //Sets lower limit of balance meter to 0
+            if (balanceLevel < 0)
+            {
+                balanceLevel = 0;
+            }
+
+            //Sets upper limit of balance meter to 100
+            if (balanceLevel > 100)
+            {
+                balanceLevel = 100;
             }
         }
     }
@@ -78,29 +121,48 @@ public class DrinkingGame : MonoBehaviour
 
         spillageAmount = 0f;
 
+        StartingTankards();
+        amountLeft = 100f;
+
         EventManager.current.ShowTimer();
+
+        gameIsSetup = true;
+    }
+
+    private void ClearDrink()
+    {
+        IsGolden.RemoveAt(0);
+
+        for (int i = 1; i < 5; i++)
+        {
+            currentListValue = IsGolden[i];
+            IsGolden.RemoveAt(i);
+            IsGolden.Insert(i - 1, currentListValue);
+        }
     }
 
     private void NextDrink()
     {
-        currentState = BalanceState.Idle;
+        balanceLevel = 0f;
         
-        amountLeft = 100;
+        amountLeft = 100f;
 
         TankardGenerator();
     }
 
-    //Generates tankard order
-    private void TankardGenerator()
+    //Generates the first 5 tankards
+    private void StartingTankards()
     {
-        for (int i = 1; i < 5; i++)
+        for (int i = 1; i < 6; i++)
         {
+            Debug.Log($"Starting Tankard #{i} has been generated");
+
             //This will pick a random number, but by having the max be a variable it will allow for the chances to be easily changed
             randomNum = Random.Range(1, randomMax);
 
             if (randomNum == 1)
             {
-                isGolden = false;
+                IsGolden.Insert((i - 1), false);
 
                 //Increases the count everytime a tankard isnt chosen to be golden
                 sinceGolden += 1;
@@ -110,7 +172,7 @@ public class DrinkingGame : MonoBehaviour
             }
             else
             {
-                isGolden = true;
+                IsGolden.Insert((i - 1), true);
 
                 //Since a golden tankard was chosen, the count is reset
                 sinceGolden = 0;
@@ -118,6 +180,34 @@ public class DrinkingGame : MonoBehaviour
                 //Any time a tankard is selected to be golden, the next one is guaranteed to be normal
                 randomMax = 1;
             }
+        }
+    }
+
+    //Generates next tankard
+    private void TankardGenerator()
+    {
+        //This will pick a random number, but by having the max be a variable it will allow for the chances to be easily changed
+        randomNum = Random.Range(1, randomMax);
+
+        if (randomNum == 1)
+        {
+            IsGolden.Insert(4, false);
+
+            //Increases the count everytime a tankard isnt chosen to be golden
+            sinceGolden += 1;
+
+            //Every time a normal tankard is chosen, the chance for a golden tankard increases by 20%
+            randomMax = (5 - sinceGolden);
+        }
+        else
+        {
+            IsGolden.Insert(4, true);
+
+            //Since a golden tankard was chosen, the count is reset
+            sinceGolden = 0;
+
+            //Any time a tankard is selected to be golden, the next one is guaranteed to be normal
+            randomMax = 1;
         }
     }
 
